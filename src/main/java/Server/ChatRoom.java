8087839de0fd44092c058cc97a956ea03e676322
pasinduSudesh <sudeshdilshan.17@cse.mrt.ac.lk>
2022-03-06 00:10:37 +0530

    added log4j2

--- src/main/java/Server/ChatRoom.java
+++ src/main/java/Server/ChatRoom.java
@@ -1,9 +1,23 @@
 package Server;
 
+import java.util.ArrayList;
+import ClientHandler.User;
+
 public class ChatRoom {
 	
 	private String roomName; //unique name
-	private String owner;
+	private User owner;
+	private ArrayList<User> userListinRoom;
+	
+	public ChatRoom(String roomName) {
+		this.roomName = roomName;
+		this.userListinRoom = new ArrayList<User>();
+	}
+	
+	public ChatRoom(String roomName, User user) {
+		this.roomName = roomName;
+		this.owner = owner;
+	}
 	
 	
 
