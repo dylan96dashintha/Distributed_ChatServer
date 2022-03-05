85638b4f55b07ff52b3213940169eeb681ee5de1
dylan96dashintha <djdilan09@gmail.com>
2022-03-05 23:48:46 +0530

    multiple client connections

---
+++ src/main/java/App.java
@@ -0,0 +1,54 @@
+
+import java.io.*;  
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner; 
+import org.json.JSONObject;
+import org.json.JSONException;
+import ClientHandler.ClientHandler;
+import Connection.ClientServerConnection;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    	try {
+    		ServerSocket ss = new ServerSocket(4444);
+    		while (true) {
+    			System.out.println("Run wenawada");
+        		Socket s=ss.accept();
+        		InputStream inputFromClient = s.getInputStream();
+        		System.out.println("Run wenawasasasaaaaaaaaaaaaa");
+        		ClientServerConnection clientServerConnection = new ClientServerConnection(inputFromClient);
+        		clientServerConnection.start();
+        	}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+    	
+    	}
+//        try{  
+//        	ServerSocket ss=new ServerSocket(4444);  
+//        	Socket s=ss.accept();
+//        	InputStream inputFromClient = s.getInputStream();
+//            Scanner scanner = new Scanner(inputFromClient, String.valueOf(StandardCharsets.UTF_8));
+//            while (true) {
+//                String line = scanner.nextLine();
+//                System.out.println("Line == "+line);
+//                ClientHandler clientHandler = new ClientHandler(getType(line));
+//                clientHandler.getTypeFunctionality();
+//                
+//            }
+//           // ss.close();  
+//        	}catch(Exception e){System.out.println(e);}  
+//      }
+//    
+//    protected static JSONObject getType(String line) {
+//    	JSONObject jsnObj = new JSONObject(line);
+//        //String type = jsnObj.getString("type");
+//        return jsnObj;
+//    }
+    
+}
