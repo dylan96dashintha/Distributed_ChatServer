7ae186dc5403ac1edafc59c86a54402ade971cce
pasinduSudesh <sudeshdilshan.17@cse.mrt.ac.lk>
2022-03-06 11:19:19 +0530

    Added server state.

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -5,9 +5,42 @@ import java.util.ArrayList;
 //functionalities of server
 public class Server {
 	
-	private int serverid;
+
+
 	private String serverName;
-	private ArrayList<ChatRoom> chatRoomList = new  ArrayList<ChatRoom>();
-	private ArrayList<String> clients = new ArrayList<String>();
+	private String serverAddress;
+	private int serverPort, clientPort;
+	
+
+	public Server(String serverName, String serverAddress, int serverPort, int clientPort) {
+		this.serverName = serverName;
+		this.serverAddress = serverAddress;
+		this.serverPort = serverPort;
+		this.clientPort = clientPort;
+	}
+	public String getServerName() {
+		return serverName;
+	}
+	public void setServerName(String serverName) {
+		this.serverName = serverName;
+	}
+	public String getServerAddress() {
+		return serverAddress;
+	}
+	public void setServerAddress(String serverAddress) {
+		this.serverAddress = serverAddress;
+	}
+	public int getServerPort() {
+		return serverPort;
+	}
+	public void setServerPort(int serverPort) {
+		this.serverPort = serverPort;
+	}
+	public int getClientPort() {
+		return clientPort;
+	}
+	public void setClientPort(int clientPort) {
+		this.clientPort = clientPort;
+	}
 
 }
